3bc92f910f3defe16c757ed03dd9ff56050d2fd7
Akankshatiwari19 <113931489+Akankshatiwari19@users.noreply.github.com>
2023-09-12 16:33:34 +0530

    Add files via upload

---
+++ Akanksha.java
@@ -0,0 +1,19 @@
+package project1;
+import java.util.Scanner;
+
+public class Akanksha {
+	public static void main(String[] args)
+	{
+
+	//1. System.out.println("Hello World);
+	
+		/*2. Scanner s1 = new Scanner(System.in);
+		int x=s1.nextInt();
+		System.out.println("Input value is "+x);*/
+		
+		
+		
+	
+		
+}
+}
