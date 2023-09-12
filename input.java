c56df783f4f59cacd80a974f83b073ea4db369f1
Akankshatiwari19 <113931489+Akankshatiwari19@users.noreply.github.com>
2023-09-12 16:36:37 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,14 @@
+package project1;
+import java.util.Scanner;
+
+public class input {
+
+	public static void main(String[] args)
+	{
+
+	
+	 Scanner s1 = new Scanner(System.in);
+		int x=s1.nextInt();
+		System.out.println("Input value is "+x);
+}
+}
